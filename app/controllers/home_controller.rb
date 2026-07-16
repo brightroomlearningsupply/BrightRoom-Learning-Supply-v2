8fafb5c5086450b6591a36122fc59b54f42c8d03
Stilyan Dimitrov <sti4o_21@aol.com>
2026-07-16 13:28:26 -0500

    polishing; adding link to donation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
     def page
         @location = "https://www.google.com/maps/place/6025+Stage+Rd+42+153,+Bartlett,+TN+38134/data=!4m2!3m1!1s0x887f82bad174bc8f:0xb5e8fd52560f1fd1?sa=X&ved=1t:242&ictx=111"
+        @donation = "https://www.paypal.com/ncp/payment/AT9ZWYYSE6RCU"
         render :home
     end 
 
